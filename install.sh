3c84a64f2e19c3172f43248bd1ed4156c86baadc
Jaxilian <jax.carls@gmail.com>
2025-01-15 15:43:46 +0100

    updt

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-sudo dnf install python3 python3-pip python3-venv certbot python3-certbot-nginx
+sudo dnf install python3 python3-pip certbot python3-certbot-nginx
 sudo dnf install nginx
 sudo certbot --nginx -d febbler.com
 systemctl enable nginx.service
 
+pip3 install --break-system-packages virtualenv
+pip install django gunicorn
 python3 -m venv venv
 source venv/bin/activate
-pip install django gunicorn
