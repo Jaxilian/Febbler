68212cc3fa59e65251e24c74d5e4fb96b57fd3ab
Jaxilian <jax.carls@gmail.com>
2025-03-12 13:50:37 +0700

    removal

--- install.sh
+++
@@ -1,9 +0,0 @@
-sudo dnf install python3 python3-pip certbot python3-certbot-nginx
-sudo dnf install nginx
-sudo certbot --nginx -d febbler.com
-systemctl enable nginx.service
-
-pip3 install --break-system-packages virtualenv
-pip install django gunicorn
-python3 -m venv venv
-source venv/bin/activate
